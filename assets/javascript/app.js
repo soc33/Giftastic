208bafef015d8d0ac189963e7ad8ee5543453cc7
soc33 <soc33@msn.com>
2019-01-03 03:22:44 -0800

    adds some flare and finished assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
-var choices = ["spongebob", "rugrats", "catdog", "ren and stimpy", "gorilla"];
+var choices = ["Spongebob", "Rugrats", "Catdog", "Ren and Stimpy", "Dexter's Lab"];
 
 function makeButtons() {
     $("#place-of-buttons").empty();
 
-    for (i=0; i < choices.length; i++) {
+    for (i = 0; i < choices.length; i++) {
         var g = $("<button>");
         g.addClass("gif");
         g.attr("data-name", choices[i]);
         g.text(choices[i]);
+        g.addClass("bg-green hover:bg-green-dark text-red font-bold py-2 px-4 rounded-full choices");
         $("#place-of-buttons").append(g);
     }
 }
@@ -19,11 +20,14 @@ $("#add-choice").on("click", function (event) {
     makeButtons();
 });
 
+// var ratingChoice = $("#grid-state").val();
 
 function displaygifs() {
     $("#gif-view").empty();
     var gifs = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=FKljlk5rvmmXmg6Kp4KEDE9ihDrYT43y&limit=10";
+    var ratingChoice = $("#grid-state").val();
+    console.log(ratingChoice);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifs + "&api_key=FKljlk5rvmmXmg6Kp4KEDE9ihDrYT43y&rating=" + ratingChoice;
 
     $.ajax({
         url: queryURL,
@@ -31,20 +35,42 @@ function displaygifs() {
     }).then(function (response) {
         console.log(response);
 
-        var div = $("<div>");
-        for (i=0; i < 10; i++) {
-            var gifRating = response.data[i].rating;
-            var giffRated = $("<p>").text("Rated: " + gifRating);
-            div.append(giffRated);
-            var giff = response.data[i].images.original.url;
-            var gifImg = $("<img>").attr("src", giff);
-            div.append(gifImg);
+        var divMain = $("<div>");
+        var m = 0;
+        for (i = 0; i < 10; i++) {
+            var div = $("<div>");
+            div.addClass("float-left divBox");
+            if (response.data[i] == undefined) {
+                break
+            } else {
+                var gifRating = response.data[i].rating;
+                var giffRated = $("<p>").text("Rated: " + gifRating);
+                div.append(giffRated);
+                var pic = response.data[i].images.fixed_height_still.url;
+                var giff = response.data[i].images.fixed_height.url;
+                var gifImg = $("<img>").attr("src", pic);
+                gifImg.attr("data-orig", giff);
+                gifImg.attr("data-still", pic);
+                gifImg.addClass("pausePlay");
+                div.append(gifImg);
+                divMain.append(div);
+            }
         }
 
-        $("#gif-view").append(div);
+
+        $("#gif-view").append(divMain);
     })
 
 }
+$(document).on("click", ".pausePlay", stopOrGo);
+function stopOrGo() {
+    if ($(this).attr("src") === $(this).attr("data-orig")) {
+        $(this).attr("src", $(this).attr("data-still"));
+    } else {
+        $(this).attr("src", $(this).attr("data-orig"));
+    }
+};
+
 $(document).on("click", ".gif", displaygifs);
 
 makeButtons();
